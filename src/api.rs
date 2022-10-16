418e140a5d84f85d6c40d3a986955e7f25d866cc
Anton Österberg <anton.osterberg@protonmail.com>
2022-10-16 23:45:52 +0200

    more warp

--- src/api.rs
+++ src/api.rs
@@ -15,14 +15,7 @@ type DbHandle = Arc<RwLock<Database>>;
 pub fn routes(
     db: DbHandle,
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-    let get_flag = warp::get()
-        .and(warp::path!("flags" / Flag))
-        .and(with_db(db))
-        .map(|flag: Flag, db: DbHandle| db.read().unwrap().get(&flag).unwrap_or_default())
-        .map(|rate: f64| {
-            let json = json!({ "rate": rate });
-            warp::reply::json(&json)
-        });
+    let get_flag = get_flag(db.clone());
     let get_all_flags = warp::get().and(warp::path("flags")).map(|| Ok("foo"));
     let head_flags = warp::head().and(warp::path("flags")).map(|| "foo");
 
@@ -37,4 +30,23 @@ pub fn routes(
 
 fn with_db(db: DbHandle) -> impl Filter<Extract = (DbHandle,), Error = Infallible> + Clone {
     warp::any().map(move || db.clone())
+}
+
+fn get_flag(
+    db: DbHandle,
+) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    warp::get()
+        .and(warp::path!("flags" / Flag))
+        .and(with_db(db))
+        .map(|flag: Flag, db: DbHandle| db.read().unwrap().get(&flag).unwrap_or_default())
+        .map(|rate: f64| {
+            let json = json!({ "rate": rate });
+            warp::reply::json(&json)
+        })
+}
+
+fn get_all_flags(
+    db: DbHandle,
+) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    warp::get().and(warp::path("flags")).map(|| Ok("foo"))
 }

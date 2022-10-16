878fac6b80706038f86557367fdbcb70e879347d
Anton Österberg <anton.osterberg@protonmail.com>
2022-10-16 15:25:56 +0200

    Partial working with warp

---
+++ src/api.rs
@@ -0,0 +1,40 @@
+use std::{
+    collections::HashMap,
+    convert::Infallible,
+    sync::{Arc, RwLock},
+};
+
+use http::Response;
+use serde_json::json;
+use warp::{reply::Json, Filter};
+
+use crate::{Database, Flag, FlagConf};
+
+type DbHandle = Arc<RwLock<Database>>;
+
+pub fn routes(
+    db: DbHandle,
+) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    let get_flag = warp::get()
+        .and(warp::path!("flags" / Flag))
+        .and(with_db(db))
+        .map(|flag: Flag, db: DbHandle| db.read().unwrap().get(&flag).unwrap_or_default())
+        .map(|rate: f64| {
+            let json = json!({ "rate": rate });
+            warp::reply::json(&json)
+        });
+    let get_all_flags = warp::get().and(warp::path("flags")).map(|| Ok("foo"));
+    let head_flags = warp::head().and(warp::path("flags")).map(|| "foo");
+
+    let put_flag = warp::put()
+        .and(warp::path!("flags" / Flag))
+        .and(warp::body::json())
+        .map(|p: Flag, b: FlagConf| format!("P: {}, B: {:?} ", p, b));
+
+    let all = get_flag.or(get_all_flags).or(head_flags).or(put_flag);
+    all
+}
+
+fn with_db(db: DbHandle) -> impl Filter<Extract = (DbHandle,), Error = Infallible> + Clone {
+    warp::any().map(move || db.clone())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,31 @@
+mod api;
+
 use std::{
     collections::HashMap,
     fmt::Display,
     str::FromStr,
-    sync::{Arc, Mutex},
+    sync::{Arc, Mutex, RwLock},
 };
 
+use http::Response;
 use lazy_static::lazy_static;
 use regex::{Regex, RegexBuilder};
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    let db = Arc::new(Mutex::new(Database::new()));
-    let get_flag = warp::get()
-        .and(warp::path!("flags" / FlagKey))
-        .map(move |flag: FlagKey| format!("Got flag {:?}", db.lock().unwrap().get(&flag)));
-
-    let get_all_flags = warp::get()
-        .and(warp::path("flags"))
-        .map(|| String::from("Get ALL flags"));
-
-    let head_flags = warp::head().and(warp::path("flags")).map(|| "foo");
-
-    let put_flag = warp::put()
-        .and(warp::path!("flags" / String))
-        .and(warp::body::json())
-        .map(|p: String, b: Flag| format!("P: {}, B: {:?} ", p, b));
-
-    let all = get_flag.or(get_all_flags).or(head_flags).or(put_flag);
-    warp::serve(all).run(([127, 0, 0, 1], 8080)).await;
+    let db = Arc::new(RwLock::new(Database::new()));
+    let api = api::routes(db);
+    warp::serve(api).run(([127, 0, 0, 1], 8080)).await;
 }
 
 #[derive(serde::Deserialize, Debug)]
-pub struct Flag {
+pub struct FlagConf {
     pub rate: f64,
 }
 
-struct Database {
-    data: HashMap<FlagKey, f64>,
+pub struct Database {
+    data: HashMap<Flag, f64>,
 }
 
 impl Database {
@@ -50,23 +38,23 @@ impl Database {
         self.data.iter().map(|(k, v)| (k.to_string(), *v)).collect()
     }
 
-    pub fn get(&self, flag: &FlagKey) -> Option<f64> {
+    pub fn get(&self, flag: &Flag) -> Option<f64> {
         self.data.get(flag).cloned()
     }
 
-    pub fn set(&mut self, flag: FlagKey, rate: f64) {
+    pub fn set(&mut self, flag: Flag, rate: f64) {
         self.data.insert(flag, rate);
     }
 
-    pub fn delete(&mut self, flag: &FlagKey) {
+    pub fn delete(&mut self, flag: &Flag) {
         self.data.remove(flag);
     }
 }
 
 #[derive(Hash, PartialEq, Eq, Clone)]
-struct FlagKey(String);
+pub struct Flag(String);
 
-impl Display for FlagKey {
+impl Display for Flag {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_str(&self.0)
     }
@@ -81,12 +69,12 @@ lazy_static! {
         .expect("Compile regex");
 }
 
-impl FromStr for FlagKey {
+impl FromStr for Flag {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match REGEX.is_match(s) {
-            true => Ok(FlagKey(s.to_string())),
+            true => Ok(Flag(s.to_string())),
             false => Err(()),
         }
     }

ba789951d0b52dbaf8c15925461d686b71328b4b
Anton Österberg <anton.osterberg@protonmail.com>
2023-06-15 23:19:35 +0200

    Add some help texts

--- src/cfg.rs
+++ src/cfg.rs
@@ -1,14 +1,28 @@
 use clap::Parser;
 
 #[derive(Parser)]
+#[command(name = "flagpole")]
 pub struct Config {
-    #[arg(short, long, env = "PORT", default_value = "3000")]
-    port: u16,
+    /// Address to bind to
     #[arg(short = 'H', long, env = "HOST", default_value = "0.0.0.0")]
     host: String,
+
+    /// Port number
+    ///
+    /// Port number to listen on for incoming requests
+    #[arg(short, long, env = "PORT", default_value = "3000")]
+    port: u16,
+    /// Log level
+    ///
+    /// Set the minimum log level
     #[cfg(feature = "logging")]
     #[arg(short, long, env = "LOG_LEVEL", default_value = "Info")]
     log_level: log::Level,
+
+    /// API key
+    ///
+    /// Set an optional API key. If set, it must be present in any requests that alters state.
+    /// If no API key is set, all requests can be done without any form of authentication.
     #[arg(short = 'K', long, env = "API_KEY")]
     api_key: Option<String>,
 }
